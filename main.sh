3249676c72c2fa6fff5ea872050423584d2d9f51
Ian Warburton <ian@fruitandashape.com>
2022-02-20 18:32:24 +0000

    Update start-up scripts.

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 rm -rf  /home/runner/dotnet
 mkdir -p  /home/runner/dotnet
-cd /home/runner/dotnet
+cd ~/dotnet
 
 wget https://download.visualstudio.microsoft.com/download/pr/43f3a3bd-3df2-41e6-beca-3ec4952ca6c4/30fe7779249607d1bb3bb4b20d61a479/dotnet-sdk-3.0.103-linux-x64.tar.gz
 
@@ -9,4 +9,6 @@ tar -zxf dotnet-sdk-3.0.103-linux-x64.tar.gz
 export DOTNET_ROOT=/home/runner/dotnet
 export PATH=$PATH:/home/runner/dotnet
 
-~/dotnet/dotnet run --project ~/TicTacToe/TicTacToe/Console.csproj
+cd -
+
+dotnet run --project TicTacToe/Console.csproj
